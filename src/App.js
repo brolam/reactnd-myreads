ed5eecd0b1b9090e4a0203123e151929b56f6114
Breno Marques <breno@brolam.com.br>
2017-09-20 11:58:34 -0300

    feat: App TDD - renders 3 list of bookshelf.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React from 'react'
 import './App.css'
-import BookShelfChanger from './components/BookShelfChanger.js';
-import BookShelfImage from './components/BookShelfImage.js';
-import BookShelf from './components/BookShelf.js';
 import BookShelfList from './components/BookShelfList.js';
 
 class BooksApp extends React.Component {
@@ -53,98 +50,8 @@ class BooksApp extends React.Component {
             <div className="list-books-content">
               <div>
                <BookShelfList listTitle="Currently Readin" books={this.state.books} onChangeBookShelf={this.onChangeBookShelf} />
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Currently Reading</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                      <BookShelf book={ {id:"nggnmAEACAAJ", title: "The Linux Command Line", authors: ["William E. Shotts", "Breno Marques" ], shelf:"currentlyReading"}} onChangeBookShelf={this.onChangeBookShelf} />
-                      <li>
-                        <div className="book">
-                          <div className="book-top">
-                            <BookShelfImage bookId="PGR2AwAAQBAJ" />
-                            <BookShelfChanger selectedOption="currentlyReading" onChangeBookShelf={this.onChangeBookShelf} />
-                          </div>
-                          <div className="book-title">To Kill a Mockingbird</div>
-                          <div className="book-authors">Harper Lee</div>
-                        </div>
-                      </li>
-                      <li>
-                        <div className="book">
-                          <div className="book-top">
-                            <BookShelfImage bookId="yDtCuFHXbAYC" />
-                            <BookShelfChanger selectedOption="currentlyReading" onChangeBookShelf={this.onChangeBookShelf}/>
-                          </div>
-                          <div className="book-title">Ender's Game</div>
-                          <div className="book-authors">Orson Scott Card</div>
-                        </div>
-                      </li>
-                    </ol>
-                  </div>
-                </div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Want to Read</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                      <li>
-                        <div className="book">
-                          <div className="book-top">
-                            <BookShelfImage bookId="uu1mC6zWNTwC" />
-                            <BookShelfChanger selectedOption="wantToRead" onChangeBookShelf={this.onChangeBookShelf}/>
-                          </div>
-                          <div className="book-title">1776</div>
-                          <div className="book-authors">David McCullough</div>
-                        </div>
-                      </li>
-                      <li>
-                        <div className="book">
-                          <div className="book-top">
-                            <BookShelfImage bookId="wrOQLV6xB-wC"/>
-                            <BookShelfChanger selectedOption="wantToRead" onChangeBookShelf={this.onChangeBookShelf}/>
-                          </div>
-                          <div className="book-title">Harry Potter and the Sorcerer's Stone</div>
-                          <div className="book-authors">J.K. Rowling</div>
-                        </div>
-                      </li>
-                    </ol>
-                  </div>
-                </div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Read</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                      <li>
-                        <div className="book">
-                          <div className="book-top">
-                            <BookShelfImage bookId="pD6arNyKyi8C" />
-                            <BookShelfChanger selectedOption="read" onChangeBookShelf={this.onChangeBookShelf} />
-                          </div>
-                          <div className="book-title">The Hobbit</div>
-                          <div className="book-authors">J.R.R. Tolkien</div>
-                        </div>
-                      </li>
-                      <li>
-                        <div className="book">
-                          <div className="book-top">
-                            <BookShelfImage bookId="1q_xAwAAQBAJ"/>
-                            <BookShelfChanger selectedOption="read" onChangeBookShelf={this.onChangeBookShelf} />
-                          </div>
-                          <div className="book-title">Oh, the Places You'll Go!</div>
-                          <div className="book-authors">Seuss</div>
-                        </div>
-                      </li>
-                      <li>
-                        <div className="book">
-                          <div className="book-top">
-                          <BookShelfImage bookId="32haAAAAMAAJ"/>
-                              <BookShelfChanger selectedOption="read" onChangeBookShelf={this.onChangeBookShelf} />
-                          </div>
-                          <div className="book-title">The Adventures of Tom Sawyer</div>
-                          <div className="book-authors">Mark Twain</div>
-                        </div>
-                      </li>
-                    </ol>
-                  </div>
-                </div>
+               <BookShelfList listTitle="Want to Read" books={this.state.books} onChangeBookShelf={this.onChangeBookShelf} />
+               <BookShelfList listTitle="Read" books={this.state.books} onChangeBookShelf={this.onChangeBookShelf} />
               </div>
             </div>
             <div className="open-search">
--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,15 +1,60 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from '../../App.js'
-
+import { mount, shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
 
 const BooksAPIMock = {}
 
-BooksAPIMock.getAll = () => new Promise(function(then) {
-  then([]);
+BooksAPIMock.getAll = () => new Promise(function (then) {
+  then(books);
 });
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
   ReactDOM.render(<App booksAPI={BooksAPIMock} />, div)
 })
+
+test('renders 3 list of bookshelf', () => {
+  const app = mount(<App booksAPI={BooksAPIMock} />);
+  const innerNode = app.find('.bookshelf');
+  expect(innerNode.length).toEqual(3);
+})
+
+const books = [{
+  id: "nggnmAEACAAJ",
+  title: "The Linux Command Line",
+  authors: ["William E. Shotts", "Breno Marques"],
+  shelf: "currentlyReading"
+},
+{
+  id: "PGR2AwAAQBAJ",
+  title: "To Kill a Mockingbird",
+  authors: ["Harper Lee", "Breno Marques"],
+  shelf: "currentlyReading"
+},
+{
+  id: "yDtCuFHXbAYC",
+  title: "Ender's Game",
+  authors: ["Orson Scott Card", "Breno Marques"],
+  shelf: "wantToRead"
+},
+{
+  id: "uu1mC6zWNTwC",
+  title: "1776",
+  authors: ["David McCullough", "Breno Marques"],
+  shelf: "wantToRead"
+},
+{
+  id: "wrOQLV6xB",
+  title: "Harry Potter and the Sorcerer's Stone",
+  authors: ["J.K. Rowlingh", "Breno Marques"],
+  shelf: "read"
+},
+{
+  id: "pD6arNyKyi8C",
+  title: "The Hobbit",
+  authors: ["J.K. Rowlingh", "Breno Marques"],
+  shelf: "read"
+}
+]
