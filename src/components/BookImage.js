242bcd2c7d0b39ac9cc6cd0af96b54e905357363
Breno Marques <breno@brolam.com.br>
2017-10-03 12:41:31 -0300

    fix: BookImage.js - Udacity code review.

--- src/components/BookImage.js
+++ src/components/BookImage.js
@@ -3,10 +3,15 @@ import PropTypes from 'prop-types'
 
 function BookImage(props) {
     return (
-        <div className="book-cover" style={{ width: 120, height: 190, backgroundImage: `url("https://books.google.com/books/content?id=${props.bookId}&printsec=frontcover&img=1&zoom=1")` }}></div>
+        <div
+            className="book-cover"
+            style={{ width: 120, height: 190, backgroundImage: `url("https://books.google.com/books/content?id=${props.bookId}&printsec=frontcover&img=1&zoom=1")` }}>
+        </div>
     )
 }
+
 BookImage.propTypes = {
     bookId: PropTypes.string.isRequired,
 }
+
 export default BookImage
