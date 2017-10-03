335bb7865a0d5e5f310b5e48079ffba923eeb4a9
Breno Marques <breno@brolam.com.br>
2017-10-03 12:33:05 -0300

    fix: BookSearch.js -- Udacity code review.

--- src/components/BookSearch.js
+++ src/components/BookSearch.js
@@ -6,22 +6,26 @@ function BookSearch(props) {
     return (
         <div className="search-books">
             <div className="search-books-bar">
-                <a id="closeSearchButton" className="close-search" onClick={() => { props.goHome() }}>Close</a>
+                <a
+                    id="closeSearchButton"
+                    className="close-search"
+                    onClick={props.goHome}>
+                    Close
+                </a>
                 <div className="search-books-input-wrapper">
-                    {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                    <input type="text" placeholder="Search by title or author" onChange={(e) => { props.search(e.target.value); }} />
-
+                    <input
+                        type="text"
+                        placeholder="Search by title or author"
+                        onChange={e => props.search(e.target.value)}
+                    />
                 </div>
             </div>
             <div className="search-books-results">
-                <BookShelf shelfTitle="Results" books={props.booksFound} onChangeBookShelf={props.onChangeBookShelf} />
+                <BookShelf
+                    shelfTitle="Results"
+                    books={props.booksFound}
+                    onChangeBookShelf={props.onChangeBookShelf}
+                />
             </div>
         </div>
     );
