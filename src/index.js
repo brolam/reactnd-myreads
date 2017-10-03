befc413dd807a8abea8702f7e69503fc31ae2b1b
Breno Marques <breno@brolam.com.br>
2017-10-03 12:44:43 -0300

    fix: index.js - Udacity code review.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import './index.css'
 import * as BooksAPI from './BooksAPI'
-import {BrowserRouter} from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 
-ReactDOM.render(<BrowserRouter><App booksAPI={BooksAPI} /></BrowserRouter>, document.getElementById('root'))
+ReactDOM.render(
+    <BrowserRouter>
+        <App
+            booksAPI={ BooksAPI }
+        />
+    </BrowserRouter>,
+    document.getElementById('root')
+)
